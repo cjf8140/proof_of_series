d57e32b07e4f50791b28c08b93ed380fc8931f0c
cjf8140 <cjf8140@gmail.com>
2021-06-15 00:45:03 +0900

    mobile friendly

---
+++ formob.js
@@ -0,0 +1,43 @@
+var gb;
+var a;
+
+var y = 20;
+
+var sum = 0;
+var lsum = -1;
+
+var run = 1;
+
+var bd = 1;
+
+var n = 0;
+function setup() {
+  createCanvas(480, 2000);
+  background(230);
+  a = Number(prompt("첫째항", "1"));
+  gb = Number(prompt("공비 입력", "0.5"));
+  stroke(2);
+  textSize(10);
+  fill(0);
+  text("Expected Value: " + a/(1-gb) ,10, 20);
+}
+ //<>//
+function draw() { //<>//
+  if(run && bd) {
+  y+=20;
+  n+=1;
+  sum+=a;
+  if(sum - lsum < 0.0000000000001) {
+    run = 0;
+  }
+  text(n+": "+"sum= "+ sum + ", n= " +a, 10, y);
+  a *= gb;
+  lsum = sum;
+}
+}
+
+function keyPressed() {
+  if(key == '1') {
+    bd = 1
+  }
+}
