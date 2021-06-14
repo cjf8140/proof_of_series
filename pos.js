2840c5121d1d25a59b258414dbf7cd1cd77635ed
cjf8140 <cjf8140@gmail.com>
2021-06-15 00:32:20 +0900

    ..

--- pos.js
+++ pos.js
@@ -10,7 +10,7 @@ var run = 1;
 
 var n = 0;
 function setup() {
-  createCanvas(1280, 2000);
+  createCanvas(480, 2000);
   background(230);
   a = Number(prompt("첫째항", "1"));
   gb = Number(prompt("공비 입력", "0.5"));
