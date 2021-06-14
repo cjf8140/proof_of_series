82783aea33f45151c4d34de3b42d9df391199778
cjf8140 <cjf8140@gmail.com>
2021-06-15 00:10:30 +0900

    .

--- pos.js
+++ pos.js
@@ -3,7 +3,7 @@ var a;
 
 var y = 0;
 
-var la = 0;
+var sum = 0;
 
 function setup() {
   createCanvas(1280, 720);
@@ -11,13 +11,14 @@ function setup() {
   a = prompt("첫째항", "1");
   gb = prompt("공비 입력", "1");
   stroke(2);
-  textSize(20);
+  textSize(10);
   fill(0);
 }
  //<>//
 function draw() { //<>//
   y+=20;
-  text("val: "+ a, 10, y);
+  sum+=a;
+  text("val: "+ sum, 10, y);
   a *= gb;
 }
 
