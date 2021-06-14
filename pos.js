ebc827077486015105e920260604ee1e0a795152
cjf8140 <cjf8140@gmail.com>
2021-06-15 00:21:10 +0900

    .

--- pos.js
+++ pos.js
@@ -4,7 +4,7 @@ var a;
 var y = 0;
 
 var sum = 0;
-
+var lsum = -1;
 function setup() {
   createCanvas(1280, 2000);
   background(230);
@@ -18,8 +18,12 @@ function setup() {
 function draw() { //<>//
   y+=20;
   sum+=a;
+  if(lsum == sum) {
+    return;
+  }
   text("sum= "+ sum + ", n= " +a, 10, y);
   a *= gb;
+  lsum = sum;
 }
 
 function keyPressed() {
